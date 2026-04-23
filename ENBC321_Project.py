5f30bd2cb5794f59cdf37921beb629d7bc59ae50
Knartey <teyteyk11@gmail.com>
2026-04-23 01:54:51 -0400

    Project preprocessing and models are now in separate files along with necessary variables from the preprocessing file

--- ENBC321_Project.py
+++ ENBC321_Project.py
@@ -1,30 +1,52 @@
 import pandas as pd
+import numpy as np
+import pickle
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.feature_selection import RFECV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.feature_selection import SequentialFeatureSelector
-df = pd.read_csv("ai_impact_student_performance_dataset.csv")
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 
-#Data Preprocessing:
-data = df.copy()
-x = data.drop('final_score', axis=1)
-y = data['final_score']
-#Fill NA values with the mean
-data = data.fillna(data.median(numeric_only=True, skipna=True))
+optimized_x_reg = np.load('optimized_x_reg.npy')
+optimized_x_clf = np.load('optimized_x_clf.npy')
+y_reg = np.load('y_reg.npy')
+classification_y = np.load('classification_y.npy')
+le = pickle.load(open('label_encoder.pkl', 'rb'))
 
-#Dummies for gender column
-data = pd.get_dummies(data, columns=['gender', 'grade_level', 'ai_tools_used', 'ai_usage_purpose', 'performance_category'], drop_first=True, dtype=int)
-
-#Feature selection using backward elimination
-model = RandomForestClassifier(random_state=42)
-backward_elim = SequentialFeatureSelector(model, n_feature_to_select='auto', tol=0.0, direction='backward', cv=5)
-backward_elim.fit(x, y)
 #Machine Learning Techniques (Supervised):
 
 #Linear Regression
+linearReg = LinearRegression()
+x_train, x_test, y_train, y_test = train_test_split(optimized_x_reg, y_reg, test_size = 0.2, random_state=10)
+linearModel = linearReg.fit(x_train, y_train)
+y_pred_linear = linearModel.predict(x_test)
+
+plt.scatter(y_test, y_pred_linear, color='blue', alpha=0.6, label='Predictions')
+
+min_val = min(y_test.min(), y_pred_linear.min())
+max_val = max(y_test.max(), y_pred_linear.max())
+plt.plot([min_val, max_val], [min_val, max_val], color='black', alpha=0.6, label='Predictions')
+plt.title('Linear Regression: Actual Vs. Predicted')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.legend()
+plt.show()
 
 #Logistic Regression
+class_x_train, class_x_test, class_y_train, class_y_test = train_test_split(optimized_x_clf, classification_y, test_size=0.2, random_state=10)
+logReg = LogisticRegression(max_iter=5000)
+logModel = logReg.fit(class_x_train, class_y_train)
+y_pred_log = logReg.predict(class_x_test)
 
+ConfusionMatrixDisplay.from_estimator(logModel, class_x_test, class_y_test, display_labels=le.classes_)
+plt.title("Logistic Regression - Confusion Matrix")
+plt.show()
 #Support Vector Machines (SVM)
+svm_model = SVC(kernel='linear')
 
 #Decision Trees
 
